7f4e2124604a4e774a8be936eb862494161a275b
tbalu98 <toth.balazs.e.pub@gmail.com>
2024-04-25 18:23:52 +0200

    red fact display

--- Exe_JT_problem_red_facts.py
+++ Exe_JT_problem_red_facts.py
@@ -123,27 +123,6 @@ for x in JT_int.H_int.eigen_kets[0:10]:
     print(str(round(x.eigen_val.real,4)) + ' meV') 
 
 
-X_op = JT_int.system_tree.create_operator('X_orb','point_defect','orbital_system')
-Y_op = JT_int.system_tree.create_operator('Y_orb', 'point_defect', 'orbital_system')
-
-X_vib_op = JT_int.system_tree.create_operator('X', 'point_defect', 'mode_1')
-Y_vib_op = JT_int.system_tree.create_operator('Y', 'point_defect', 'mode_1')
-
-print('Expected vals')
-
-for i in range(0, len(JT_int.H_int.eigen_kets[0:10])):
-
-    eigen_ket = JT_int.H_int.eigen_kets[i]
-
-    print(str(i) + ' eigen ket:')
-    print(X_op.calc_expected_val(eigen_ket))
-    print(Y_op.calc_expected_val(eigen_ket))
-
-    print(X_vib_op.calc_expected_val(eigen_ket))
-    print(Y_vib_op.calc_expected_val(eigen_ket))
-
-print('-------------------------------')
-
 
 #print( [  x.eigen_val for x in JT_int.H_int.eigen_kets ] )
 
@@ -217,9 +196,11 @@ kramers_12 = JT_int.get_base_state().create_ket_vector([  ket_state_1, ket_state
 kramers_32 = JT_int.get_base_state().create_ket_vector([  ket_state_3, ket_state_4])
 """
 
-l = 34.6
-if l!=0.0:
+#l = 34.6
 
+if l!=0.0:
+    print()
+    print('spin orbit coupling lambda = ' + str(l))
     point_defect_tree.insert_node('electron_system', spin_sys)
 
     #JT_int = qmp.Exe_tree(point_defect_tree, JT_theory)
@@ -238,13 +219,7 @@ if l!=0.0:
     LzSz_expected_vals= [ LzSz_op.calc_expected_val( eig_ket) for eig_ket in JT_int.H_int.eigen_kets[0:50] ]
     
     eigen_energies = [ x.eigen_val for x in JT_int.H_int.eigen_kets[0:50]]
-    """
-    for i in range(0, 50):
-        LzSz_exp = LzSz_op.calc_expected_val(JT_int.H_int.eigen_kets[i])
-        #print(LS_exp)
-        LzSz_expected_vals.append(LzSz_exp)
-        #print(JT_int.H_int.calc_expected_val(JT_int.H_int.eigen_kets[i]))
-    """
+
 
     p_32 = LzSz_expected_vals[2]
     p_12 = LzSz_expected_vals[0]
@@ -257,9 +232,16 @@ if l!=0.0:
     p = p_32+p_12
     delta = p_32-p_12
 
-    print('C8 and C9 equation:'  )
+    print('Based on C8 and C9 equation:'  )
     print("p = " + str( p ))
     print( "delta = " + str(delta) )
+
+    print('According to figure:')
+    
+    print(  "p = " + str(p_32-p_12))
+    
+
+    print( "delta = " + str( p_32+p_12))
     plt.rcParams['font.size'] = 18
     plt.title('PbV energy and spin-orbit coupling \n expectation value')
     plt.xlabel(r'$\left< L_{z}S_{z} \right>$')
@@ -279,118 +261,6 @@ if l!=0.0:
     print('p Ham reduction factor')
 
     print( 'lambda_Ham = ' + str(round((JT_int.H_int.eigen_kets[2].eigen_val- JT_int.H_int.eigen_kets[0].eigen_val).real,4)) + ' meV')
-
-
-    X_op = JT_int.system_tree.create_operator('Lz','point_defect','orbital_system')
-    Y_op = JT_int.system_tree.create_operator('Y_orb', 'point_defect', 'orbital_system')
-
-    X_vib_op = JT_int.system_tree.create_operator('X', 'point_defect', 'mode_1')
-
-    Y_vib_op = JT_int.system_tree.create_operator('Y', 'point_defect', 'mode_1')
-
-    spin_op = JT_int.system_tree.create_operator('Sz','point_defect', 'spin_system')
-
-    print('X position expected val')
-
-    orb_pos_x_vals = []
-    spin_z_vals = []
-
-    for i in range(0, len(JT_int.H_int.eigen_kets[0:10])):
-    #for i in range(0, 10):
-
-        eigen_ket = JT_int.H_int.eigen_kets[i]
-
-        orb_pos_x_val = X_op.calc_expected_val(eigen_ket)
-        orb_pos_y_val = Y_op.calc_expected_val(eigen_ket)
-
-        nuc_x_val = X_vib_op.calc_expected_val(eigen_ket)
-        nuc_y_val = Y_vib_op.calc_expected_val(eigen_ket)
-
-        orb_pos_x_vals.append(orb_pos_x_val)
-
-        spin_z_val = spin_op.calc_expected_val(eigen_ket)
-
-        spin_z_vals.append(spin_z_val)
-
-        print(str(i) + ' eigen ket:')
-
-
-        print( 'orbital x position ' +  str(orb_pos_x_val))
-        print( 'orbital y position ' +  str(orb_pos_y_val))
-
-        print( 'spin z ' + str(spin_z_val))
-        print( 'nuclei x position ' + str( nuc_x_val))
-        print( 'nuclei y position ' + str( nuc_y_val))
-
-        print('-------------------------------')
-
-
-    
-    base_ket_vec:mf.ket_vector = JT_int.system_tree.root_node.base_states.create_ket_vector( [ ket_state_1, ket_state_2, ket_state_3, ket_state_4   ]  )
-
-    base_ket_vec = base_ket_vec.normalize()
-
-    eigen_energies = []
-
-    vals = []
-
-    for i in range(0, len( JT_int.H_int.eigen_kets[0:10])):
-        eig_ket_vec:mf.ket_vector = JT_int.H_int.eigen_kets[i]
-        eigen_energies.append( eig_ket_vec.eigen_val.real)
-        
-        coeff = (base_ket_vec.to_bra_vector()*eig_ket_vec)
-        val = abs(coeff)**2
-        vals.append(val)
-        spin_z_vals[i] = spin_z_vals[i]*val
-
-    """
-    plt.plot(eigen_energies, vals, 'x')
-    plt.show()
-    print(base_ket_vec)
-    """
-"""
-plt.plot(orb_pos_x_vals,spin_z_vals, 'x')
-plt.show()
-"""
-delta = 1.0
-f = 1.0
-
-
-if Bz!=0.0 or Bx!= 0.0 or By != 0.0:
-    JT_int.system_tree.insert_node('electron_system', spin_sys)
-
-    JT_int.create_one_mode_hamiltonian()
-
-    JT_int.add_spin_orbit_coupling(l) if l>0.0 else None
-
-    JT_int.add_magnetic_field(Bx,By,Bz, delta, f)
-
-    JT_int.H_int.calc_eigen_vals_vects()
-
-    print('p Ham reduction factor')
-
-    print( 'lambda_0 = ' + str(round((JT_int.H_int.eigen_kets[2].eigen_val- JT_int.H_int.eigen_kets[0].eigen_val).real,4)) + ' meV')
-
-
-
-
-if E_x !=0.0 or E_y != 0.0:
-
-    JT_int.create_one_mode_hamiltonian()
-
-    JT_int.add_electric_field(E_x, E_y)
-    JT_int.H_int.calc_eigen_vals_vects()
-    print('-------------------------------')
-
-    print('q Ham reduction factor')
-
-    if JT_int.system_tree.find_subsystem('spin_system') == None:
-        print( 'lambda_0 = ' + str(round((JT_int.H_int.eigen_kets[1].eigen_val- JT_int.H_int.eigen_kets[0].eigen_val).real,4)) + ' meV')
-    else:
-        print( 'lambda_0 = ' + str(round((JT_int.H_int.eigen_kets[3].eigen_val- JT_int.H_int.eigen_kets[0].eigen_val).real,4)) + ' meV')
-
-
-JT_int.save_eigen_vals_vects_to_file(res_folder + calc_name+'_eigen_vecs.csv',res_folder+ calc_name+'_eigen_vals.csv')
 
 
 
